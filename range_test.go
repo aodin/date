01b9a6630de60e741d0c3ed25ecbe910ab53f075
Aaron O. Ellis <aaronoellis@gmail.com>
2016-02-02 22:00:45 -0700

    Range tests no longer use testify

--- range_test.go
+++ range_test.go
@@ -3,137 +3,228 @@ package date
 import (
 	"encoding/json"
 	"testing"
+)
 
-	"github.com/stretchr/testify/assert"
+var (
+	empty     = Empty()
+	year2015  = EntireYear(2015)
+	nov       = EntireMonth(2015, 11)
+	dec       = EntireMonth(2015, 12)
+	novOnward = Range{Start: nov.Start}
+	decOnward = Range{Start: dec.Start}
+	untilDec  = Range{End: nov.End}
+	jan       = EntireMonth(2016, 1)
+	feb       = EntireMonth(2016, 2)
 )
 
 func TestRange(t *testing.T) {
-	assert.True(t, Infinity().IsInfinity())
-	assert.True(t, Empty().IsEmpty())
+	if !Infinity().IsInfinity() {
+		t.Error("Infinity should be infinity")
+	}
+	if !Empty().IsEmpty() {
+		t.Error("Empty should be empty")
+	}
+	if !Never().Equals(Empty()) {
+		t.Error("Never should equal Empty")
+	}
+	if !Forever().Equals(Infinity()) {
+		t.Error("Forever should equal Infinity")
+	}
 
-	assert.True(t, Never().Equals(Empty()))
-	assert.True(t, Forever().Equals(Infinity()))
-	assert.False(t, Forever().Equals(Empty()))
+	if Forever().Equals(Empty()) {
+		t.Error("Forever should not equal Empty")
+	}
 }
 
 func TestRange_Contains(t *testing.T) {
-	year2015 := EntireYear(2015)
-	dec := EntireMonth(2015, 12)
-	novOnward := Range{Start: New(2015, 11, 1)}
-	decOnward := Range{Start: New(2015, 12, 1)}
+	if !year2015.Contains(dec) {
+		t.Error("year 2015 should contain December 2015")
+	}
+	if !dec.DoesNotContain(year2015) {
+		t.Error("December 2015 should not contain the year 2015")
+	}
 
-	assert.True(t, year2015.Contains(dec))
-	assert.True(t, dec.DoesNotContain(year2015))
+	if !novOnward.Contains(dec) {
+		t.Error("November 2015 onward should contain December 2015")
+	}
+	if year2015.Contains(novOnward) {
+		t.Error("Year 2015 should not contain November 2015 onward")
+	}
 
-	assert.True(t, novOnward.Contains(dec))
-	assert.False(t, year2015.Contains(novOnward))
+	if !novOnward.Contains(novOnward) {
+		t.Error("November 2015 onward should contain itself")
+	}
 
-	assert.True(t, novOnward.Contains(novOnward))
+	if !novOnward.Contains(decOnward) {
+		t.Error("November 2015 onward should contain December 2015 onward")
+	}
+	if decOnward.Contains(novOnward) {
+		t.Error("December 2015 onward should not contain November 2015 onward")
+	}
+}
 
-	assert.True(t, novOnward.Contains(decOnward))
-	assert.False(t, decOnward.Contains(novOnward))
+var daysTests = []struct {
+	want, have int
+}{
+	{0, Empty().Days()},
+	{1, OnlyToday().Days()},
+	{365, EntireYear(2015).Days()},
+	{366, EntireYear(2016).Days()},
+	{29, EntireMonth(2016, 2).Days()},
 }
 
 func TestRange_Days(t *testing.T) {
-	assert.Equal(t, 0, Empty().Days())
-	assert.Equal(t, 1, OnlyToday().Days())
-	assert.Equal(t, 365, EntireYear(2015).Days())
-	assert.Equal(t, 366, EntireYear(2016).Days())
-	assert.Equal(t, 29, EntireMonth(2016, 2).Days())
+	for _, test := range daysTests {
+		if test.want != test.have {
+			t.Errorf("Range Days() want=%d have=%d", test.want, test.have)
+		}
+	}
 }
 
 func TestRange_Error(t *testing.T) {
-	assert.Nil(t, Never().Error())
+	if Never().Error() != nil {
+		t.Error("Never should not error")
+	}
 
 	// Unbounded ranges are allowed
 	end := Range{End: New(2015, 3, 1)}
-	assert.Nil(t, end.Error(), "Unbounded start dates should not error")
+	if end.Error() != nil {
+		t.Error("Unbounded start dates should not error")
+	}
 
 	start := Range{Start: New(2015, 3, 1)}
-	assert.Nil(t, start.Error(), "Unbounded end dates should not error")
+	if start.Error() != nil {
+		t.Error("Unbounded end dates should not error")
+	}
 
 	var invalid Range
 	invalid.Start = New(2015, 3, 2)
 	invalid.End = New(2015, 3, 1)
-	assert.NotNil(t, invalid.Error())
+	if invalid.Error() == nil {
+		t.Error("Invalid ranges should error")
+	}
+}
+
+var intersectionTests = []struct {
+	want, have Range
+}{
+	{dec, novOnward.Intersection(dec)},
+	{nov, year2015.Intersection(nov)},
+	{decOnward, decOnward.Intersection(novOnward)},
+	{decOnward, novOnward.Intersection(decOnward)},
+	{nov, novOnward.Intersection(untilDec)},
 }
 
 func TestRange_Intersection(t *testing.T) {
-	year2015 := EntireYear(2015)
-	nov := EntireMonth(2015, 11)
-	dec := EntireMonth(2015, 12)
-	novOnward := Range{Start: nov.Start}
-	decOnward := Range{Start: dec.Start}
-	untilDec := Range{End: nov.End}
-	empty := Empty()
+	if !empty.Intersection(nov).IsZero() {
+		t.Error("An empty range should have a zero intersection")
+	}
 
-	assert.True(t, empty.Intersection(nov).IsZero())
-	assert.Equal(t, dec, novOnward.Intersection(dec))
-
-	assert.Equal(t, nov, year2015.Intersection(nov))
-
-	assert.Equal(t, decOnward, decOnward.Intersection(novOnward))
-	assert.Equal(t, decOnward, novOnward.Intersection(decOnward))
-	assert.Equal(t, nov, novOnward.Intersection(untilDec))
+	for _, test := range intersectionTests {
+		if test.want != test.have {
+			t.Errorf(
+				"Range Intersection() want=%v have=%v", test.want, test.have,
+			)
+		}
+	}
 }
 
 func TestRange_Overlaps(t *testing.T) {
-	year2015 := EntireYear(2015)
-	nov := EntireMonth(2015, 11)
-	dec := EntireMonth(2015, 12)
-	novOnward := Range{Start: New(2015, 11, 1)}
-
-	assert.False(t, nov.Overlaps(dec))
-	assert.True(t, dec.Overlaps(year2015))
-	assert.True(t, nov.Overlaps(SingleDay(New(2015, 11, 30))))
-	assert.True(t, novOnward.Overlaps(dec))
+	if nov.Overlaps(dec) {
+		t.Error("November should not overlap December")
+	}
+	if !dec.Overlaps(year2015) {
+		t.Error("December 2015 should overlap the year 2015")
+	}
+	if !nov.Overlaps(SingleDay(New(2015, 11, 30))) {
+		t.Error("November 2015 should overlap 2015-11-30")
+	}
+	if !novOnward.Overlaps(dec) {
+		t.Error("November 2015 onward should overlap December 2015")
+	}
 }
 
 func TestRange_Marshal(t *testing.T) {
 	// Empty ranges should render as null
 	b, err := json.Marshal(Never())
-	assert.Nil(t, err)
-	assert.Equal(t, "null", string(b))
+	if err != nil {
+		t.Fatal("json.Marshal of Never() should not error")
+	}
+	if string(b) != "null" {
+		t.Error(`json.Marshal of Never() should be null`)
+	}
 
 	// Infinite ranges should render as null start and end dates
 	b, err = json.Marshal(Infinity())
-	assert.Nil(t, err)
-	assert.Equal(t, `{"start":null,"end":null}`, string(b))
+	if err != nil {
+		t.Fatal("json.Marshal of Infinity() should not error")
+	}
+	if string(b) != `{"start":null,"end":null}` {
+		t.Error(
+			`json.Marshal of Infinity() should be {"start":null,"end":null}`,
+		)
+	}
+}
+
+var stringTests = []struct {
+	want, have string
+}{
+	{"never", Never().String()},
+	{"forever", Forever().String()},
+	{"2016-02-01 to 2016-02-29", EntireMonth(2016, 2).String()},
+	{"until 2016-02-29", Range{End: New(2016, 2, 29)}.String()},
+	{"2016-02-01 onward", Range{Start: New(2016, 2, 1)}.String()},
 }
 
 func TestRange_String(t *testing.T) {
-	assert.Equal(t, "never", Never().String())
-	assert.Equal(t, "forever", Forever().String())
-	assert.Equal(t, "2016-02-01 to 2016-02-29", EntireMonth(2016, 2).String())
-	assert.Equal(t, "until 2016-02-29", Range{End: New(2016, 2, 29)}.String())
-	assert.Equal(t, "2016-02-01 onward", Range{Start: New(2016, 2, 1)}.String())
+	for _, test := range stringTests {
+		if test.want != test.have {
+			t.Errorf("Range String() want=%s have=%s", test.want, test.have)
+		}
+	}
 }
 
 func TestRange_Union(t *testing.T) {
-	year2015 := EntireYear(2015)
-	jan := EntireMonth(2016, 1)
 	union := year2015.Union(jan)
+	if New(2015, 1, 1) != union.Start {
+		t.Error(
+			"The union of 2015 and January 2016 should start on 2015-01-01",
+		)
+	}
+	if New(2016, 1, 31) != union.End {
+		t.Error(
+			"The union of 2015 and January 2016 should end on 2016-01-31",
+		)
+	}
 
-	assert.Equal(t, New(2015, 1, 1), union.Start)
-	assert.Equal(t, New(2016, 1, 31), union.End)
-
-	feb := EntireMonth(2016, 2)
 	union = jan.Union(feb)
-	assert.Equal(t, feb.End, union.End)
-	assert.Equal(t, jan.Start, union.Start)
+	if jan.Start != union.Start {
+		t.Error(
+			"The union of January and February 2016 should start on 2016-01-01",
+		)
+	}
+	if feb.End != union.End {
+		t.Error(
+			"The union of January and February 2016 should end on 2016-02-29",
+		)
+	}
 
-	nov := EntireMonth(2015, 11)
-	dec := EntireMonth(2015, 12)
-	novOnward := Range{Start: nov.Start}
-	decOnward := Range{Start: dec.Start}
-	untilDec := Range{End: nov.End}
+	if decOnward.Union(novOnward) != novOnward {
+		t.Error("The union of November onward and December onward should be Novermber onward")
+	}
+	if untilDec.Union(decOnward) != Forever() {
+		t.Error("The union of until December and December onward should be forever")
+	}
 
-	assert.Equal(t, novOnward, decOnward.Union(novOnward))
-	assert.Equal(t, Forever(), untilDec.Union(decOnward))
-
-	assert.Equal(t, Empty(), Empty().Union(Empty()))
-	assert.Equal(t, Forever(), Empty().Union(Forever()))
-	assert.Equal(t, Forever(), Forever().Union(Empty()))
+	if Empty().Union(Empty()) != Empty() {
+		t.Error("The union of two empty ranges should be empty")
+	}
+	if Empty().Union(Forever()) != Forever() {
+		t.Error("The union of a forever range should be forever")
+	}
+	if Forever().Union(Empty()) != Forever() {
+		t.Error("The union of a forever range should be forever")
+	}
 }
 
 func TestRange_Unmarshal(t *testing.T) {
@@ -141,13 +232,24 @@ func TestRange_Unmarshal(t *testing.T) {
 	open := EntireMonth(2015, 2)
 
 	raw := `{"start":"2015-03-01","end":null}`
-	assert.Nil(t, json.Unmarshal([]byte(raw), &open))
-	assert.Equal(t, New(2015, 3, 1), open.Start)
-	assert.True(t, open.End.IsZero())
+	if json.Unmarshal([]byte(raw), &open) != nil {
+		t.Error("json.Unmarshal should not error")
+	}
+
+	if New(2015, 3, 1) != open.Start {
+		t.Error("The start date after json.Unmarshal should be 2015-03-01")
+	}
+	if !open.End.IsZero() {
+		t.Error("The end date after json.Unmarshal should be zero")
+	}
 
 	// TODO nulls should be unmarshaled as empty ranges
 	// raw = `null`
 	// var zero Range
-	// assert.Nil(t, json.Unmarshal([]byte(raw), &zero))
-	// assert.True(t, zero.IsEmpty())
+	// if json.Unmarshal([]byte(raw), &zero) != nil {
+	// 	t.Error("json.Unmarshal should not error for null ranges")
+	// }
+	// if !zero.IsEmpty() {
+	// 	t.Error("null ranges after json.Unmarshal should be empty")
+	// }
 }
